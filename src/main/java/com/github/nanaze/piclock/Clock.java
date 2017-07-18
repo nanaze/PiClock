8660bcc0fade6739f84d1ba17e261325c26eda5d
Nathan Naze <nanaze@gmail.com>
2017-07-18 11:01:22 -0700

    reformat source

--- src/main/java/com/github/nanaze/piclock/Clock.java
+++ src/main/java/com/github/nanaze/piclock/Clock.java
@@ -24,82 +24,86 @@ import java.time.LocalTime;
 
 
 class ClockCanvas extends Canvas {
-    public ClockCanvas() {
-	setBackground(Color.BLACK);
+  public ClockCanvas() {
+    setBackground(Color.BLACK);
+  }
+
+
+  public void paint(Graphics g) {
+    g.setColor(Color.WHITE);
+
+    LocalTime time = LocalTime.now();
+    String timeString = String.format("%d:%02d:%02d", time.getHour(), time.getMinute(), time.getSecond());
+
+    drawString(timeString, g);
+  }
+
+  private void drawString(String timeString, Graphics g) {
+    Dimension dim = getSize();
+
+    int fontSize = 1;
+    Font font = new Font(Font.MONOSPACED, Font.PLAIN, fontSize);
+    Font biggestFont = font;
+
+    // Figure out the biggest font that fits.
+    while (true) {
+      font = new Font(Font.MONOSPACED, Font.PLAIN, fontSize);
+      FontMetrics fm = g.getFontMetrics(font);
+      Rectangle2D rect = fm.getStringBounds(timeString, g);
+
+      if (rect.getWidth() <= dim.width &&
+        rect.getHeight() <= dim.height) {
+        biggestFont = font;
+      } else {
+        break;
+      }
+
+      fontSize++;
     }
-    
-    public void paint(Graphics g) {
-	g.setColor(Color.WHITE);
 
-	LocalTime time = LocalTime.now();
-	String timeString = String.format("%d:%02d:%02d", time.getHour(), time.getMinute(), time.getSecond());
-	
-	drawString(timeString, g);
-    }
-
-    private void drawString(String timeString, Graphics g) {
-	Dimension dim = getSize();
-
-	int fontSize = 1;
-	Font font = new Font(Font.MONOSPACED, Font.PLAIN, fontSize);	
-	Font biggestFont = font;
-
-	// Figure out the biggest font that fits.
-	while (true) {
-	    font = new Font(Font.MONOSPACED, Font.PLAIN, fontSize);
-	    FontMetrics fm = g.getFontMetrics(font);
-	    Rectangle2D rect = fm.getStringBounds(timeString, g);
-
-	    if (rect.getWidth() <= dim.width &&
-		rect.getHeight() <= dim.height) {
-		biggestFont = font;
-	    } else {
-		break;
-	    }
-
-	    fontSize++;
-	}
-
-	// Draw the string.
-	g.setFont(biggestFont);
-	Rectangle2D rect = g.getFontMetrics().getStringBounds(timeString, g);
-	g.drawString(timeString,
-		     (int)(dim.width - rect.getWidth()) / 2,
-		     (int)(dim.height - rect.getHeight()) / 2 + (int) Math.abs(rect.getY()));
-    }
+    // Draw the string.
+    g.setFont(biggestFont);
+    Rectangle2D rect = g.getFontMetrics().getStringBounds(timeString, g);
+    g.drawString(timeString,
+      (int) (dim.width - rect.getWidth()) / 2,
+      (int) (dim.height - rect.getHeight()) / 2 + (int) Math.abs(rect.getY()));
+  }
 }
 
 class ClockKeyListener implements KeyListener {
-    public void keyPressed(KeyEvent e) {
-	if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-	    System.exit(0);
-	}
+  public void keyPressed(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+      System.exit(0);
     }
+  }
 
-    public void keyReleased(KeyEvent e) {}
-    public void keyTyped(KeyEvent e) {}
+  public void keyReleased(KeyEvent e) {
+  }
+
+  public void keyTyped(KeyEvent e) {
+  }
 }
 
 class Clock {
 
-    public static void main(String[] args) throws InterruptedException {
-	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-	Frame frame = new Frame();
-	Canvas canvas = new ClockCanvas();
-	frame.add(canvas);
+  public static void main(String[] args) throws InterruptedException {
+    GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    Frame frame = new Frame();
+    Canvas canvas = new ClockCanvas();
+    frame.add(canvas);
 
-	KeyListener kl = new ClockKeyListener();
-	frame.addKeyListener(kl);
+    KeyListener kl = new ClockKeyListener();
+    frame.addKeyListener(kl);
 
-	gd.setFullScreenWindow(frame);
+    gd.setFullScreenWindow(frame);
 
-	TimerTask task = new TimerTask(){
-		public void run() {
-		    canvas.repaint();
-		}
-	    };
+    TimerTask task = new TimerTask() {
+      public void run() {
+        canvas.repaint();
+      }
+    };
 
-	Timer timer = new Timer();
-	timer.scheduleAtFixedRate(task, 0, 1000);
-    }
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(task, 0, 1000);
+  }
 }
